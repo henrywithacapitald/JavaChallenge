417dba6a058b02e2755041b240a9c8c2b3b7d86b
Henry <95125800+henrywithacapitald@users.noreply.github.com>
2021-12-15 11:14:23 +1300

    Add files via upload

---
+++ YourInitials.java
@@ -0,0 +1,14 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println("H   H  EEEEE  N    N   RRRR   Y   Y ");
+        System.out.println("H   H  E      NN   N  R    R  Y   Y ");
+        System.out.println("H   H  E      N  N N  R    R   Y Y");
+        System.out.println("HHHHH  EEE    N   NN  RRRRR     Y");
+        System.out.println("H   H  E      N    N  R R       Y");
+        System.out.println("H   H  E      N    N  R   R     Y");
+        System.out.println("H   H  EEEEE  N    N  R     R   Y");
+        
+
+    }
+}
